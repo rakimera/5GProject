934ebb4837b0c5e3addf06e19fc7ae805224ea2e
Artur <zelih@list.ru>
2023-07-13 02:47:41 +0600

    #13 - Прокинул подключение в ConfigureService для работы с jwt токеном - Создал класс AuthenticationOptions  где прописал данные по токену - В контроллере прописал логику получения токена и проверки юзера

--- WebApi/Controllers/AccountController.cs
+++ WebApi/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Application.Common;
 using Application.Interfaces;
 using Application.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-
-namespace WebApi.Controllers;
+using Microsoft.IdentityModel.Tokens;
 
 [ApiController]
 [Route("api/users")]
@@ -46,4 +48,54 @@ public class AccountController : Controller
     [HttpDelete]
     public async Task<IActionResult> Delete(Guid oid) =>
         Ok(await _service.UserService.Delete(oid));
+
+    [HttpPost("/token")]
+    public IActionResult Token(string login, string password)
+    { 
+        var identity = GetIdentity(login, password); 
+        if (identity == null)
+        {
+            return BadRequest(new { errorText = "Неправильное имя пользователя или пароль." });
+        }
+        var now = DateTime.UtcNow; 
+        var jwt = new JwtSecurityToken(
+            issuer: AuthenticationOptions.ISSUER,
+            audience: AuthenticationOptions.AUDIENCE,
+            notBefore: now,
+            claims: identity.Claims,
+            expires: now.Add(TimeSpan.FromMinutes(AuthenticationOptions.LIFETIME)),
+            signingCredentials: new SigningCredentials(AuthenticationOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+        var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+        var response = new
+        {
+            access_token = encodedJwt,
+            login = identity.Name
+        };
+        return Ok(response); 
+    }
+    private ClaimsIdentity GetIdentity(string login, string password) 
+    {
+        try
+        {
+            UserDTO user = _service.UserService.GetAuthorizedUser(login,password).Result;
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role)
+                };
+                ClaimsIdentity claimsIdentity =
+                    new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                        ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+            return null;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
 }
