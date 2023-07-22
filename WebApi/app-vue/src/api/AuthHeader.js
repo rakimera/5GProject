3cb608d1d4ebddc4e16ec137f45b9dc44c75b156
Nikita <nikita.dronoff@gmail.com>
2023-07-22 21:27:29 +0600

    #18 Вынес логику обновления токена из AuthHeader.js

--- WebApi/app-vue/src/api/AuthHeader.js
+++ WebApi/app-vue/src/api/AuthHeader.js
@@ -1,19 +1,8 @@
-import authService from "@/api/AuthService";
 export default async function authHeader() {
     try {
         const user = JSON.parse(localStorage.getItem('userToken'));
         if (user && user.accessToken) {
             return { Authorization: 'Bearer ' + user.accessToken };
-        } else {
-            const user = JSON.parse(localStorage.getItem('refreshToken'));
-            const tokenApiModel = { accessToken: user.accessToken, refreshToken: user.refreshToken };
-            try {
-                const response = await authService.refreshingToken(tokenApiModel);
-                return { Authorization: 'Bearer ' + response.data.accessToken };
-            } catch (error) {
-                console.error('Ошибка обновления токена:', error);
-                return {};
-            }
         }
     } catch (error) {
         throw error;
--- WebApi/app-vue/src/utils/axios.js
+++ WebApi/app-vue/src/utils/axios.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
 import authHeader from './authHeader';
+import authService from "@/api/AuthService";
 
 const instance = axios.create({
     baseURL: 'https://localhost:7015/',
 });
 axios.defaults.headers.common = authHeader();
+axios.interceptors.response.use(response => {
+    return response;
+}, async error => {
+    if (error.response.status === 401) {
+        const user = JSON.parse(localStorage.getItem('refreshToken'));
+        const tokenApiModel = {accessToken: user.accessToken, refreshToken: user.refreshToken};
+        try {
+            const response = await authService.refreshingToken(tokenApiModel);
+            localStorage.getItem('userToken')
+            localStorage.getItem('refreshToken')
+        } catch (error) {
+            console.error('Ошибка обновления токена:', error);
+            return {};
+        }
+        return error;
+    }});
 
 export default instance;
