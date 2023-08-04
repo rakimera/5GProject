ae6228a9a7c4129d75261443e29057f27c3ce31b
Artur <zelih@list.ru>
2023-08-04 10:12:27 +0600

    #11 Прописал конфиг для бд

--- Infrastructure/Persistence/DataContext/Project5GDbContext.cs
+++ Infrastructure/Persistence/DataContext/Project5GDbContext.cs
@@ -1,3 +1,4 @@
+using Application.Models.ContrAgents;
 using Domain.Common;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -22,12 +23,51 @@ public class Project5GDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<BaseEntity>().HasQueryFilter(x => x.IsDelete == false);
         modelBuilder.Entity<User>().HasIndex(x => x.Login).IsUnique();
-        modelBuilder.Entity<BaseEntity>()
+        modelBuilder.Entity<ContrAgent>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<User>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<District>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<Town>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<Antenna>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<EnergyResult>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<Location>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<Project>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<ProjectAntenna>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<ProjectStatus>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<TranslatorSpecs>().HasQueryFilter(x => x.IsDelete == false);
+        modelBuilder.Entity<User>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<ContrAgent>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<District>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<Town>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<Project>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<Antenna>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<EnergyResult>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<Location>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<ProjectAntenna>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<ProjectStatus>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        modelBuilder.Entity<TranslatorSpecs>()
             .Property(b => b.Created)
             .HasDefaultValueSql("CURRENT_TIMESTAMP");
         base.OnModelCreating(modelBuilder);
-        
     }
 }
