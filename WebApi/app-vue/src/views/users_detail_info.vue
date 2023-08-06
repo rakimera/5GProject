5334c34936c8a600e9a04b006e2767d302b250f9
Nikita <nikita.dronoff@gmail.com>
2023-08-06 15:01:56 +0600

    #9 Рефакторинг, синтаксический сахар

--- WebApi/app-vue/src/views/users_detail_info.vue
+++ WebApi/app-vue/src/views/users_detail_info.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h2>Подробнее о пользователе</h2>
-
     <dx-form
         id="form"
         label-location="top"
@@ -9,84 +8,60 @@
         :disabled="isFormDisabled">
     </dx-form>
       <DxButton
-              
               text="Редактировать"
-              type="normal"
-              styling-mode="contained"
               :on-click="onClickEditUser"
               v-if="isFormDisabled"
       />
       <DxButton
-              
               text="Подтвердить"
-              type="normal"
-              styling-mode="contained"
               :on-click="onClickSaveChanges"
               v-if="!isFormDisabled"
       />
   </div>
 </template>
-
-<script>
-
+<script setup>
 import DxForm from "devextreme-vue/form";
 import DxButton from 'devextreme-vue/button';
 import {onBeforeMount, reactive, ref} from "vue";
 import userService from "@/api/userService";
 import {useRoute} from "vue-router";
 
-export default {
-  setup(){
-    const route = useRoute();
-    const dataSource = reactive({
-        login: "",
-        name: "",
-        surname: "",
-        role: "",
-    });
-    let isFormDisabled = ref(true);
-    const oid = route.params.id;
-    const mode = route.params.mode;
+const route = useRoute();
+const dataSource = reactive({
+    login: "",
+    name: "",
+    surname: "",
+    role: "",
+});
+let isFormDisabled = ref(true);
+const oid = route.params.id;
+const mode = route.params.mode;
 
-    onBeforeMount(async () => {
-        console.log(oid + " <======= oid")
-        console.log(mode + " <======= mode")
-        const response = await userService.getUser(oid);
-        dataSource.login = response.data.result.login;
-        dataSource.name = response.data.result.name;
-        dataSource.surname = response.data.result.surname;
-        dataSource.role = response.data.result.role;
-    })
-    function onClickEditUser() {
-        isFormDisabled.value = false;
-    }
-    async function onClickSaveChanges() {
-        const updatedData = {
-            id: oid,
-            login: dataSource.login,
-            name: dataSource.name,
-            surname: dataSource.surname,
-            role: dataSource.role,
-        };
-        try {
-            await userService.updateUser(updatedData);
-            isFormDisabled.value = true;
-        } catch (error) {
-            console.error("Ошибка при сохранении изменений:", error);
-        }
-    }
-      
-    return {
-        dataSource,
-        isFormDisabled,
-        onClickEditUser,
-        onClickSaveChanges,
+onBeforeMount(async () => {
+    console.log(oid + " <======= oid")
+    console.log(mode + " <======= mode")
+    const response = await userService.getUser(oid);
+    dataSource.login = response.data.result.login;
+    dataSource.name = response.data.result.name;
+    dataSource.surname = response.data.result.surname;
+    dataSource.role = response.data.result.role;
+})
+function onClickEditUser() {
+    isFormDisabled.value = false;
+}
+async function onClickSaveChanges() {
+    const updatedData = {
+        id: oid,
+        login: dataSource.login,
+        name: dataSource.name,
+        surname: dataSource.surname,
+        role: dataSource.role,
     };
-  },
-    components: {
-        DxForm,
-        DxButton,
-    },
-};
+    try {
+        await userService.updateUser(updatedData);
+        isFormDisabled.value = true;
+    } catch (error) {
+        console.error("Ошибка при сохранении изменений:", error);
+    }
+}
 </script>
-
