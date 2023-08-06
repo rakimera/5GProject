9cc856421048182bb953bff81a098f0c105bf9fc
Nikita <nikita.dronoff@gmail.com>
2023-08-06 14:57:30 +0600

    #9 Рефакторинг на Composition

--- WebApi/app-vue/src/views/users_detail_info.vue
+++ WebApi/app-vue/src/views/users_detail_info.vue
@@ -8,80 +8,85 @@
         :form-data="dataSource"
         :disabled="isFormDisabled">
     </dx-form>
-    
-    <button @click="editUser" v-if="isFormDisabled">Редактировать</button>
-
-    <button @click="saveChanges" v-if="!isFormDisabled">Подтвердить</button>
-
+      <DxButton
+              
+              text="Редактировать"
+              type="normal"
+              styling-mode="contained"
+              :on-click="onClickEditUser"
+              v-if="isFormDisabled"
+      />
+      <DxButton
+              
+              text="Подтвердить"
+              type="normal"
+              styling-mode="contained"
+              :on-click="onClickSaveChanges"
+              v-if="!isFormDisabled"
+      />
   </div>
 </template>
 
 <script>
 
-import userService from "@/api/userService";
 import DxForm from "devextreme-vue/form";
-import {reactive} from "vue";
+import DxButton from 'devextreme-vue/button';
+import {onBeforeMount, reactive, ref} from "vue";
+import userService from "@/api/userService";
+import {useRoute} from "vue-router";
 
 export default {
-  components: {
-    DxForm,
-
-  },
-  data() {
+  setup(){
+    const route = useRoute();
     const dataSource = reactive({
-      login: "",
-      name: "",
-      surname: "",
-      role: "",
+        login: "",
+        name: "",
+        surname: "",
+        role: "",
+    });
+    let isFormDisabled = ref(true);
+    const oid = route.params.id;
+    const mode = route.params.mode;
+
+    onBeforeMount(async () => {
+        console.log(oid + " <======= oid")
+        console.log(mode + " <======= mode")
+        const response = await userService.getUser(oid);
+        dataSource.login = response.data.result.login;
+        dataSource.name = response.data.result.name;
+        dataSource.surname = response.data.result.surname;
+        dataSource.role = response.data.result.role;
     })
+    function onClickEditUser() {
+        isFormDisabled.value = false;
+    }
+    async function onClickSaveChanges() {
+        const updatedData = {
+            id: oid,
+            login: dataSource.login,
+            name: dataSource.name,
+            surname: dataSource.surname,
+            role: dataSource.role,
+        };
+        try {
+            await userService.updateUser(updatedData);
+            isFormDisabled.value = true;
+        } catch (error) {
+            console.error("Ошибка при сохранении изменений:", error);
+        }
+    }
+      
     return {
-      dataSource,
-      isFormDisabled: true,
+        dataSource,
+        isFormDisabled,
+        onClickEditUser,
+        onClickSaveChanges,
     };
   },
-  created() {
-    this.loadUserDetail();
-    const mode = this.$route.params.mode;
-    if (mode === "create") {
-      this.isFormDisabled = false;
-    } else {
-      this.loadUserDetail();
-    }
-  },
-  methods: {
-    async loadUserDetail() {
-      const oid = this.$route.params.id;
-      const mode = this.$route.params.mode;
-      console.log(oid + " <======= oid")
-      console.log(mode + " <======= mode")
-      const response = await userService.getUser(oid);
-      this.dataSource.login = response.data.result.login;
-      this.dataSource.name = response.data.result.name;
-      this.dataSource.surname = response.data.result.surname;
-      this.dataSource.role = response.data.result.role;
+    components: {
+        DxForm,
+        DxButton,
     },
-
-    editUser() {
-      this.isFormDisabled = false;
-    },
-
-    async saveChanges() {
-      const updatedData = {
-        id: this.$route.params.id,
-        login: this.dataSource.login,
-        name: this.dataSource.name,
-        surname: this.dataSource.surname,
-        role: this.dataSource.role,
-      };
-
-      try {
-        console.log(updatedData)
-        await userService.updateUser(updatedData);
-        this.isFormDisabled = true;
-      } catch (error) {
-        console.error("Ошибка при сохранении изменений:", error);
-      }
-    },
-  },
 };
 </script>
+
