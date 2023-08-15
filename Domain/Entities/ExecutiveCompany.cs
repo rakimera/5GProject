cfc9bdf0b0a652ee6a46b6eb1eb436d94a783fba
Nikita <nikita.dronoff@gmail.com>
2023-08-15 23:55:57 +0600

    #40 обновил сущности и связи в проекте

--- Domain/Entities/ExecutiveCompany.cs
+++ Domain/Entities/ExecutiveCompany.cs
@@ -4,10 +4,11 @@ namespace Domain.Entities;
 
 public class ExecutiveCompany : BaseEntity
 {
-    public Guid AddressId { get; set; }
-    public Address Address { get; set; }
+    public required string Address { get; set; }
     public Guid CompanyLicenseId { get; set; }
     public CompanyLicense CompanyLicense { get; set; }
     public string BIN { get; set; }
     public string CompanyName { get; set; }
+    public List<Project> Projects { get; set; }
+    public List<User> Users { get; set; }
 }
--- Domain/Entities/Project.cs
+++ Domain/Entities/Project.cs
@@ -4,17 +4,19 @@ namespace Domain.Entities;
 
 public class Project : BaseEntity
 {
+    public int ProjectNumber { get; set; }
     public Guid ContrAgentId { get; set; }
     public ContrAgent ContrAgent { get; set; }
     public Guid ExecutorId { get; set; }
     public User Executor { get; set; }
+    public Guid ExecutiveCompanyId { get; set; }
+    public ExecutiveCompany ExecutiveCompany { get; set; }
     public Guid? SanPinDockId { get; set; }
     public SanPinDock? SanPinDock { get; set; }
     public DateTime? YearOfInitial { get; set; }
     public Guid ProjectStatusId { get; set; }
     public ProjectStatus ProjectStatus { get; set; }
-    public Address Address { get; set; }
-    public Guid AddressId { get; set; }
+    public required string Address { get; set; }
     public List<ProjectAntenna> ProjectAntennae { get; set; } = new List<ProjectAntenna>();
     public List<TotalFluxDensity> TotalFluxDensity { get; set; } = new List<TotalFluxDensity>();
 }
