740e1f6de830b52f1266be4d89eba9646480631e
Nikita <nikita.dronoff@gmail.com>
2023-08-23 18:40:13 +0600

    #42 Рефакторинг создания проекта - вынес в отдельный компонент создание проекта - создал второй компонент пока заполнил его шаблонно

--- WebApi/app-vue/src/views/project-detail-info.vue
+++ WebApi/app-vue/src/views/project-detail-info.vue
@@ -19,78 +19,18 @@
                     title="Контрагент и адрес установки"
                     tabIndex=0
                 >
-                    <dx-item
-                        data-field='projectNumber'
-                        editor-type='dxTextBox'
-                        :editor-options="{ 
-                      stylingMode: 'filled',
-                      labelMode: 'floating',
-                      label: 'Номер проекта (сайт)' }"
-                    >
-                        <dx-required-rule message="Укажите Sitename(номер) проекта"></dx-required-rule>
-                        <dx-label
-                            :visible="false"
-                        />
-                    </dx-item>
-                    <dx-item
-                        data-field='contrAgentId'
-                        editor-type="dxSelectBox"
-                        :editor-options="{ 
-                        placeholder: 'Выберите контрагента', 
-                        items: contrAgents, 
-                        displayExpr: 'companyName', 
-                        valueExpr: 'id',
-                        labelMode: 'floating',
-                        label: 'Контрагент'}"
-                    >
-                        <dx-required-rule message="Вы не выбрали контрагента"></dx-required-rule>
-                        <dx-label
-                            :visible="false"
-                        />
-                    </dx-item>
-                    <dx-item
-                        data-field='townName'
-                        editor-type="dxSelectBox"
-                        :editor-options="{ 
-                        placeholder: 'Выберите город', 
-                        items: towns, 
-                        displayExpr: 'townName', 
-                        valueExpr: 'townName',
-                        labelMode: 'floating',
-                        label: 'Город'}"
-                    >
-                        <dx-required-rule message="Вы не выбрали город установки"></dx-required-rule>
-                        <dx-label
-                            :visible="false"
-                        />
-                    </dx-item>
-                    <dx-item
-                        data-field='address'
-                        editor-type='dxTextBox'
-                        :editor-options="{ 
-                      stylingMode: 'filled', 
-                      label: 'Район, улица, дом', 
-                      labelMode: 'floating' }"
-                    >
-                      <dx-label
-                          :visible="false"
-                      />
-                    </dx-item>
+                  <project-form
+                      :on-save-project="onSaveProject">
+                  </project-form>
                 </dx-tab>
                 <dx-tab
                     title="Антенны-передатчики"
                     tabIndex=1
                     :disabled="isTabDisabled"
                 >
-                    <dx-item
-                        data-field='house'
-                        editor-type='dxTextBox'
-                        :editor-options="{ stylingMode: 'filled', placeholder: 'Номер здания' }"
-                    >
-                        <dx-label
-                            :text="'Номер здания'"
-                        />
-                    </dx-item>
+                  <project-antenna-form
+                  >
+                  </project-antenna-form>
                 </dx-tab>
                 <dx-tab
                     tabIndex=2
@@ -107,57 +47,30 @@
                         />
                     </dx-item>
                 </dx-tab>
-            </dx-tabbed-item>>
-            <dx-button-item>
-                <dx-button-options
-                    width="100%"
-                    type="success"
-                    styling-mode="outlined"
-                    :template="mode === 'create' ? 'Создать и продолжить' : 'Сохранить изменения'"
-                    :on-click="onClickSaveChanges"
-                    :visible="!isFormDisabled"
-                    :use-submit-behavior="true"
-                >
-                </dx-button-options>
-            </dx-button-item>
-            <dx-button-item>
-                <dx-button-options
-                        width="100%"
-                        type="default"
-                        styling-mode="outlined"
-                        template="Редактировать"
-                        :on-click="onClickEditProject"
-                        :visible="isFormDisabled"
-                        :use-submit-behavior="true"
-                >
-                </dx-button-options>
-            </dx-button-item>
+            </dx-tabbed-item>
         </dx-form>
     </div>
 </template>
 <script setup>
 
 import {
-    DxForm,
-    DxLabel,
-    DxButtonItem,
-    DxButtonOptions,
-    DxTabbedItem,
-    DxTabPanelOptions,
-    DxTab, DxItem
+  DxForm,
+  DxLabel,
+  DxTabbedItem,
+  DxTabPanelOptions,
+  DxTab, DxItem
 } from "devextreme-vue/form";
-import {
-    DxRequiredRule
-} from 'devextreme-vue/validator';
+
 import {onBeforeMount, reactive, ref} from "vue";
 import projectService from "@/api/projectService";
-import {useRoute, /*useRouter*/} from "vue-router";
-import notify from "devextreme/ui/notify";
+import {useRoute, useRouter} from "vue-router";
 import contrAgentService from "@/api/contrAgentService";
 import townService from "@/api/townService";
+import ProjectForm from "@/components/project-form.vue";
+import ProjectAntennaForm from "@/components/project-antenna-form.vue";
 
 const route = useRoute();
-//const router = useRouter();
+const router = useRouter();
 let dataSource = reactive({});
 //const routeParams = {name: "projects"};
 let isFormDisabled = ref(true);
@@ -180,76 +93,18 @@ onBeforeMount(async () => {
   if (mode.value === "read") {
       const response = await projectService.getProject(oid);
       Object.assign(dataSource, response.data.result);
+      isTabDisabled.value = false;
   } else {
       isFormDisabled.value = false;
       pageDescription.value = "Создание проекта"
   }
 })
-function onClickEditProject() {
-    isFormDisabled.value = false;
-    isTabDisabled.value = false;
-}
-async function onClickSaveChanges() {
-    try {
-        const formInstance = formRef.value.instance;
-        const isFormValid = await formInstance.validate();
-        if (isFormValid.isValid === false) {
-            notify({
-                message: 'Данные не корректны',
-                position: {
-                    my: 'center top',
-                    at: 'center top',
-                },
-            }, 'warning', 1000);
-        }
-        else {
-            if (mode.value === "read") {
-                const responseUpdate = await projectService.updateProject(dataSource);
-                if (responseUpdate.data.success) {
-                    notify({
-                        message: 'Проект успешно отредактирован',
-                        position: {
-                            my: 'center top',
-                            at: 'center top',
-                        },
-                    }, 'success', 1000);
-                    isFormDisabled.value = true;
-                } else {
-                    notify(responseUpdate.data.messages, 'error', 2000);
-                }
-            } else {
-                const response = await projectService.createProject(dataSource);
-                if (response.data.success) {
-                    notify({
-                        message: 'Проект успешно создан',
-                        position: {
-                            my: 'center top',
-                            at: 'center top',
-                        },
-                    }, 'success', 1000);
-                    mode.value =  "read"
-                    isTabDisabled.value = false;
-                    index.value++
-                } else {
-                    notify({
-                            message: response.data.messages,
-                            position: {
-                                my: 'center top',
-                                at: 'center top'}
-                            }, 'error', 2000);
-                }
-            }
-        }
 
-    } catch (error) {
-        console.error("Ошибка при сохранении изменений:", error);
-        notify({
-            message: "Ошибка сервера при сохранении изменений:",
-            position: {
-                my: 'center top',
-                at: 'center top'}
-        }, 'error', 2000);
-    }
+async function onSaveProject(id) {
+  await router.push({name: 'projectDetail', params: {mode: "read", id: id}});
+  isTabDisabled.value = false;
+  isFormDisabled.value = true;
+  index.value++
 }
 </script>
 <style scoped>
