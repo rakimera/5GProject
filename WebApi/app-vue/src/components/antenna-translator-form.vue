6c5b1c238062225a9456ae7651d3b15b5d5074cb
Nikita <nikita.dronoff@gmail.com>
2023-08-31 23:47:44 +0600

    #60 Дополнил компонент добавления передатчиков в проект

--- WebApi/app-vue/src/components/antenna-translator-form.vue
+++ WebApi/app-vue/src/components/antenna-translator-form.vue
@@ -1,196 +1,126 @@
 <template>
-    <div>
-        <div id="form-container">
-            <dx-form
-                id="antenna-translator-form"
-                ref="formRef"
-                :col-count="1"
-                :form-data="dataSource"
-                label-location="top"
-                :read-only="isFormDisabled"
-                :show-colon-after-label="true"
-                :show-validation-summary="true">
-                    <dx-group-item
-                            item-type="group"
-                            name="phones"
-                            v-for="(antennaTranslators, i) in translatorOptions"
-                            :key="'antennaTranslators' + (i + 1)"
-                    >
-                        <dx-simple-item
-                            :data-field="'antennaTranslators[' + i + ']'"
-                            editor-type="dxSelectBox"
-                            :editor-options="{ 
-                                placeholder: 'Выберите частоту', 
-                                items: translators, 
-                                displayExpr: 'translatorSpecs.frequency', 
-                                valueExpr: 'id',
-                                labelMode: 'floating',
-                                label: 'Частота антенны' + (i + 1)}"
-                        >
-                            <dx-label :text="'Частота антенны ' + (i + 1)"/>
-                        </dx-simple-item>
-                    </dx-group-item>
-                <dx-button-item
-                        :button-options="addATranslatorButtonOptions"
-                        css-class="add-antenna-button"
-                        horizontal-alignment="left"
-                />
-                <dx-button-item>
-                    <dx-button-options
-                            width="100%"
-                            type="success"
-                            styling-mode="outlined"
-                            :template="mode === 'create' ? 'Создать и продолжить' : 'Сохранить изменения'"
-                            :on-click="onClickSaveChanges"
-                            :visible="!isFormDisabled"
-                            :use-submit-behavior="true"
-                    >
-                    </dx-button-options>
-                </dx-button-item>
-            </dx-form>
-        </div>
-    </div>
+  <div>
+    <dx-data-grid
+        :data-source="dataSource"
+        :show-borders="true"
+        :remote-operations="true"
+        :column-auto-width="true"
+        key-expr="id"
+        @row-updating="onRowUpdating"
+    >
+      <dx-editing
+          :allow-updating="true"
+          :allow-adding="true"
+          :allow-deleting="true"
+          :texts="{confirmDeleteMessage: 'Вы уверены, что хотите удалить эту запись?'}"
+          mode="form"
+      />
+      <dx-column
+          data-field="translatorSpecsId"
+          caption="Частота"
+          data-type="string"
+          alignment="left">
+        <dx-required-rule message="Вы не выбрали частоту"></dx-required-rule>
+        <dx-lookup
+            :data-source="translators"
+            value-expr="id"
+            display-expr="frequency"
+        />
+      </dx-column>
+      <dx-column
+          data-field="power"
+          data-type="number"
+          caption="Мощность"
+          :editor-options="{stylingMode: 'filled', labelMode: 'floating'}"
+          alignment="left">
+        <dx-label :visible="false"/>
+        <dx-required-rule message="Вы не заполнили мощность"></dx-required-rule>
+      </dx-column>
+      <dx-column
+          data-field="transmitLossFactor"
+          data-type="number"
+          caption="КФ потери сигнала"
+          :editor-options="{stylingMode: 'filled', labelMode: 'floating'}"
+          alignment="left">
+        <dx-label :visible="false"/>
+        <dx-required-rule message="Вы не заполнили коэффициент потери сигнала"></dx-required-rule>
+      </dx-column>
+      <dx-column
+          data-field="translatorTypeId"
+          caption="Тип передатчика"
+          data-type="string"
+          alignment="left">
+        <dx-lookup
+            :data-source="translatorTypes"
+            value-expr="id"
+            display-expr="frequency"
+        />
+      </dx-column>
+      <dx-column
+          data-field="gain"
+          data-type="number"
+          caption="КФ усиления"
+          :editor-options="{stylingMode: 'filled', labelMode: 'floating'}"
+          alignment="left">
+        <dx-required-rule message="Вы не запонели коэффициент усиления сигнала"></dx-required-rule>
+      </dx-column>
+      <dx-column
+          data-field="projectAntennaId"
+          data-type="string"
+          :visible="false">
+        <dx-form-item
+            :editor-options="{
+                disabled: true}"
+            editor-type="dxTextArea"
+            :visible="false"
+            :data="projectAntennaId"
+        />
+      </dx-column>
+      <dx-paging :page-size="5"/>
+      <dx-pager :show-page-size-selector="true" :allowed-page-sizes="[8, 12, 20]"/>
+    </dx-data-grid>
+  </div>
 </template>
 <script setup>
 import {
-    DxForm,
-    DxSimpleItem,
-    DxGroupItem,
-    DxButtonItem,
-    DxLabel, DxButtonOptions,
+    DxLabel
 } from 'devextreme-vue/form';
-import {computed, reactive, ref, /*defineProps*/} from "vue";
-import projectAntennaService from "@/api/projectAntennaService";
+import {onMounted, reactive, ref, defineProps} from "vue";
 import {useRoute} from "vue-router";
-import notify from "devextreme/ui/notify";
+import {
+  DxColumn,
+  DxDataGrid,
+  DxEditing,
+  DxFormItem,
+  DxLookup,
+  DxPager,
+  DxPaging
+} from "devextreme-vue/data-grid";
+import {DxRequiredRule} from "devextreme-vue/validator";
+import translatorService from "@/api/translatorService";
 
-/*const props = defineProps({
-    selectedAntennaId: String})*/
-let isFormDisabled = ref(true);
-const formRef = ref(null);
+const props = defineProps({
+  masterDetailData: {
+    type: Object,
+    default: () => ({}),
+  }})
+const antennaId = ref();
+const projectAntennaId = ref();
+const translatorTypes = ref();
 const route = useRoute();
 const mode = ref(route.params.mode);
 let dataSource = reactive([]);
 const translators = ref([]);
-const translatorOptions = ref(getTranslatorOptions(dataSource));
 
-const addATranslatorButtonOptions = computed(() =>{
-    return {
-        icon: 'add',
-        text: 'добавить частоту',
-        disabled: isFormDisabled.value,
-        onClick: () => {
-            dataSource.push('');
-            translatorOptions.value = getTranslatorOptions(dataSource);
-        },
-    };
-});
 
-function getTranslatorOptions(dataSource) {
-    const options = [];
-    for (let i = 0; i < dataSource.length; i += 1) {
-        options.push(generateNewTranslatorOptions(i));
-    }
-    return options;
-}
+onMounted(async () => {
+  antennaId.value = props.masterDetailData.row.antennaId;
+  projectAntennaId.value = props.masterDetailData.key
+  const response = await translatorService.getAllByAntennaId(antennaId.value)
+  const translatorTypes = await translatorService.
+  translators.value = response.data.result;
+})
 
-function generateNewTranslatorOptions(index) {
-    return {
-        buttons: [{
-            name: 'trash',
-            location: 'after',
-            options: {
-                stylingMode: 'text',
-                icon: 'trash',
-                onClick: () => {
-                    translators.value.splice(index, 1);
-                    translatorOptions.value = getTranslatorOptions(translators.value);
-                },
-            },
-        }],
-    };
-}
-
-async function onClickSaveChanges() {
-    try {
-        const formInstance = formRef.value.instance;
-        const isFormValid = await formInstance.validate();
-        if (isFormValid.isValid === false) {
-            notify({
-                message: 'Данные не корректны',
-                position: {
-                    my: 'center top',
-                    at: 'center top',
-                },
-            }, 'warning', 1000);
-        }
-        else {
-            if (mode.value === "read") {
-                const responseUpdate = await projectAntennaService.updateProjectAntenna(dataSource);
-                if (responseUpdate.data.success) {
-                    notify({
-                        message: 'Проект успешно отредактирован',
-                        position: {
-                            my: 'center top',
-                            at: 'center top',
-                        },
-                    }, 'success', 1000);
-                } else {
-                    notify(responseUpdate.data.messages, 'error', 2000);
-                }
-                isFormDisabled.value = true;
-            } else {
-                const response = await projectAntennaService.createProjectAntenna(dataSource);
-                if (response.data.success) {
-                    notify({
-                        message: 'Проект успешно создан',
-                        position: {
-                            my: 'center top',
-                            at: 'center top',
-                        },
-                    }, 'success', 1000);
-                } else {
-                    notify({
-                        message: response.data.messages,
-                        position: {
-                            my: 'center top',
-                            at: 'center top'}
-                    }, 'error', 2000);
-                }
-            }
-        }
-
-    } catch (error) {
-        console.error("Ошибка при сохранении изменений:", error);
-        notify({
-            message: "Ошибка сервера при сохранении изменений:",
-            position: {
-                my: 'center top',
-                at: 'center top'}
-        }, 'error', 2000);
-    }
-}
 </script>
 <style scoped>
-#form-container {
-    margin: 10px 10px 30px;
-}
-
-.long-title h3 {
-    font-family:
-            'Segoe UI Light',
-            'Helvetica Neue Light',
-            'Segoe UI',
-            'Helvetica Neue',
-            'Trebuchet MS',
-            Verdana;
-    font-weight: 200;
-    font-size: 28px;
-    text-align: center;
-    margin-bottom: 20px;
-}
-
-.add-antenna-button {
-    margin-top: 10px;
-}
 </style>
