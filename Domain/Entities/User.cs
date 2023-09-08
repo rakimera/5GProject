ed8e69d3734d4738c227b54cb91c4d14ab272ea8
Nikita <nikita.dronoff@gmail.com>
2023-09-08 14:31:43 +0600

    #71 Добавил новые поля в сущности

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,6 +7,8 @@ public class User : BaseEntity
     public string Login { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public string? PhoneNumber { get; set; }
     public string PasswordHash { get; set; }
     public byte[] Salt { get; set; }
     public List<RefreshToken> RefreshTokens { get; set; }
