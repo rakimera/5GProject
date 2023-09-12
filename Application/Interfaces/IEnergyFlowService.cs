19eaa81f28dcfc735cf2d3add461edf02b1611e9
Nikita <nikita.dronoff@gmail.com>
2023-09-12 19:08:52 +0600

    #65 Реализация функционала кнопки расчета - добавил контроллер для работы с сервисом и фронтом для обеспечения просчетов - поправил методику расчетов и записи их в БД

--- Application/Interfaces/IEnergyFlowService.cs
+++ Application/Interfaces/IEnergyFlowService.cs
@@ -6,8 +6,8 @@ namespace Application.Interfaces;
 
 public interface IEnergyFlowService
 {
-    Task<BaseResponse<string>> CreateAsync(CreateEnergyResultDto createEnergyResultDto, string creator);
-    BaseResponse<List<EnergyResultDto>> GetAllByOid(string oid);
+    Task<BaseResponse<string>> CreateAsync(string projectId, string creator);
+    BaseResponse<List<EnergyResultDto>> GetAllByOid(string id);
     Task<BaseResponse<bool>> Delete(string id);
     List<CreateTotalFluxDensityDto> PowerDensitySummation(List<EnergyResult> energyResult);
 }
--- WebApi/app-vue/src/components/project-export.vue
+++ WebApi/app-vue/src/components/project-export.vue
@@ -19,9 +19,39 @@
 </template>
 <script setup>
 import DxButton from "devextreme-vue/button";
+import {useRoute} from "vue-router";
+import exportService from "@/api/exportService";
+import notify from "devextreme/ui/notify";
 
+const route = useRoute();
+let id = route.params.id;
 async function calculateProject(){
-  console.log("кнопочкээээ")
+  try {
+    const response = await exportService.getEnergyFlow(id);
+    if (response.data.success){
+      notify({
+        message: 'Проект успешно просчитан',
+        position: {
+          my: 'center top',
+          at: 'center top',
+        },
+      }, 'success', 1000);
+    } else {
+      notify({
+        message: response.data.messages,
+        position: {
+          my: 'center top',
+          at: 'center top'}
+      }, 'error', 2000);
+    }
+  }catch (error){
+    notify({
+      message: "Ошибка сервера при реализации просчетов:" + error,
+      position: {
+        my: 'center top',
+        at: 'center top'}
+    }, 'error', 2000);
+  }
 }
 </script>
 <style scoped>
